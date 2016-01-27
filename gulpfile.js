ded261a9803518b6181797d2e848082fc81da6b5
Patryk Kopycinski <contact@patrykkopycinski.com>
2016-01-27 17:36:56 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var gulp 		= require('gulp'),
+	del 		= require('del'),
+	concat 		= require('gulp-concat'),
+	rename 		= require('gulp-rename'),
+	uglify 		= require('gulp-uglify'),
+	sass 		= require('gulp-sass'),
+	csso 		= require('gulp-csso'),
+	watch 		= require('gulp-watch'),
+	livereload 	= require('gulp-livereload');
+
+
+gulp.task('styles', function() {
+	return gulp.src('src/sass/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}))
+		.pipe(rename("theme.min.css"))
+		.pipe(gulp.dest('assets/css'));
+});
+
+
+gulp.task('scripts', function() {
+	return gulp.src(['src/js/plugin/*.js', 'src/js/base.js'])
+		.pipe(uglify())
+		.pipe(concat('theme.min.js'))
+		.pipe(gulp.dest('assets/js'));
+});
+
+
+gulp.task('ghost_config', ['scripts'], function() {
+	return gulp.src(['src/js/config.js', 'assets/js/theme.min.js'])
+		.pipe(concat('theme.min.js'))
+		.pipe(gulp.dest('assets/js'));
+});
+
+
+gulp.task('default', function() {
+	gulp.start('styles', 'scripts', 'ghost_config', 'watch');
+});
+
+
+gulp.task('watch', function() {
+	gulp.watch('src/sass/*.scss', ['styles']);
+	gulp.watch('src/js/*.js', ['scripts', 'ghost_config']);
+	livereload.listen();
+	gulp.watch(['*']).on('change', livereload.changed);
+});
---
+++ src/js/config.js
@@ -0,0 +1,19 @@
+var config = {
+	// Disqus Shortname (http://disqus.com)
+	// Options: Shortname / Blank ('') (Disabled)
+	'disqus_shortname': 'ECKODEV',
+	// Google Analytics ID (http://analytics.google.com)
+	// Options: ID / Blank (Disabled)
+	'analytics_id': '',
+	// Enable Syntax Highlighting of Code 
+	// Options: true / false
+	'highlightcode': true,
+	// Enable Auto-Loading Comments
+	// Options: true / false
+	'autoload_comments': false,
+	// Enable 'Theme By EckoThemes' Copyright Disclaimer
+	// Options: true / false
+	'show_ecko_disclaimer': true,
+};
+
+
